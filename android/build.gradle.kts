97e71776b750286c0535d409c2c3646701d57396
rezagasem1357 <rezagasem.82@gmail.com>
2026-09-01 21:13:13 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,20 +17,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// حل ریشه‌ای تداخل Namespace تمام پروژه‌های فرعی و AARها
-subprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("com.android.library") || plugins.hasPlugin("com.android.application")) {
-            val androidExtension = extensions.findByName("android")
-            if (androidExtension is com.android.build.gradle.BaseExtension) {
-                if (androidExtension.namespace == null) {
-                    androidExtension.namespace = "com.build.fix." + project.name.replace("-", "_")
-                }
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
