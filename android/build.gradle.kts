1e9b4aceef063b5f780354546169bf0e934cb8f0
rezagasem1357 <rezagasem.82@gmail.com>
2026-09-01 20:25:41 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,18 +17,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// حل مشکل Namespace ماژول‌های tflite در Kotlin DSL
-subprojects {
-    afterEvaluate {
-        if (extra.has("android")) {
-            val android = extra.get("android") as? com.android.build.gradle.BaseExtension
-            if (android != null && android.namespace == null) {
-                android.namespace = project.group.toString()
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
